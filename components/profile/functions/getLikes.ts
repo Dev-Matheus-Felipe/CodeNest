a90533eb7072bb9d7287205277cf49f28fd6a89c
Matheus Felipe <matheusfelipebom@gmail.com>
2025-12-29 16:21:03 -0300

    Test Build 20

--- components/profile/functions/getLikes.ts
+++ components/profile/functions/getLikes.ts
@@ -2,8 +2,8 @@ import { UserType } from "./getUser";
 
 export function GetLikes({user} : {user: UserType}){
     const userLikes = 
-        user.posts.reduce<number>((acc, post) => acc + post.likedBy.length, 0) + 
-        user.responses.reduce<number>((acc, response) => acc +  response.likedBy.length, 0);
+        user.posts.reduce<number>((acc: number, post) => acc + post.likedBy.length, 0) + 
+        user.responses.reduce<number>((acc: number, response) => acc +  response.likedBy.length, 0);
 
     return userLikes;
 }
